0c8d9901d53a61a307a6e6511474e132ce455dae
eirikhanasand <eirik.hanasand@gmail.com>
2025-09-26 00:50:17 +0200

    Added hook declaration file

---
+++ src/hooks/hooks.d.ts
@@ -0,0 +1,6 @@
+declare module 'login-linjeforeningen-for-it/hooks' {
+    export default function useVisibility<T extends HTMLElement>(
+        onVisible: () => void,
+        rootMargin?: string
+    ): { ref: RefObject<T>; isVisible: boolean }
+}
