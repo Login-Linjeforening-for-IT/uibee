007a41dfe0538324faab07460e28778272968fad
0herm <0herm@protonmail.com>
2025-10-03 23:33:32 +0200

    v1.6.8

--- dist/src/utils/auth/login.js
+++ dist/src/utils/auth/login.js
@@ -3,7 +3,7 @@ export default async function AuthLogin({ clientID, redirectURL, authURL }) {
     const state = Math.random().toString(36).substring(5);
     const authQueryParams = new URLSearchParams({
         client_id: clientID,
-        redirect_url: redirectURL,
+        redirect_uri: redirectURL,
         response_type: 'code',
         scope: 'openid profile email',
         state: state,
