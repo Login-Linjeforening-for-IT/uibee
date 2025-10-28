1646daf7512cf9c749f9acd83d1954c485e1b05b
0herm <0herm@protonmail.com>
2025-10-28 20:21:09 +0100

    add build

--- dist/src/utils/cookies/cookies.js
+++ dist/src/utils/cookies/cookies.js
@@ -1,8 +1,14 @@
 export function getCookie(name) {
-    const matches = document.cookie.match(new RegExp('(?:^|; )' + name.replace(/([.$?*|{}()[\]/\\+^])/g, '\\$1') + '=([^;]*)'));
+    if (typeof document === 'undefined')
+        return null;
+    const matches = document.cookie.match(new RegExp('(?:^|; )' +
+        name.replace(/([.$?*|{}()[\]/\\+^])/g, '\\$1') +
+        '=([^;]*)'));
     return matches ? decodeURIComponent(matches[1]) : null;
 }
 export function setCookie(name, value, days) {
+    if (typeof document === 'undefined')
+        return;
     let expires = '';
     if (!value) {
         return;
@@ -16,13 +22,15 @@ export function setCookie(name, value, days) {
         `${name}=${encodeURIComponent(value)}; ` +
             `${expires} path=/; SameSite=Lax`;
 }
+export function removeCookie(name) {
+    if (typeof document === 'undefined')
+        return;
+    document.cookie =
+        `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; ` +
+            'path=/; SameSite=Lax';
+}
 export function removeCookies(...cookies) {
     for (const cookie of cookies) {
         removeCookie(cookie);
     }
-}
-export function removeCookie(name) {
-    document.cookie =
-        `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; ` +
-            'path=/; SameSite=Lax';
 }
