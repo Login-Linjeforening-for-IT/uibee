321d0f51cb44c1470534ca66946731029febace9
eirikhanasand <eirik.hanasand@gmail.com>
2025-12-03 15:22:51 +0100

    Converted to client side and fixed double wrapper propagation issue

--- src/components/login/loginPage.tsx
+++ src/components/login/loginPage.tsx
@@ -1,9 +1,11 @@
+'use client'
+
 import { LoginPageProps } from 'uibee/components'
 import { LogIn } from 'lucide-react'
-import Link from 'next/link'
 import Logo from '@components/logo/logo'
+import Link from 'next/link'
 
-export default function LoginPage({title, description, redirectURL, version, btg, handleSubmit}: LoginPageProps) {
+export default function LoginPage({ title, description, redirectURL, version, btg, handleSubmit }: LoginPageProps) {
     return (
         <main className='w-full h-full flex items-center justify-center bg-login-800 p-8'>
             <div
@@ -11,7 +13,7 @@ export default function LoginPage({title, description, redirectURL, version, btg
                     'flex flex-col justify-center items-center bg-login-600 px-4 py-12 rounded-xl w-full max-w-md gap-4 md:gap-6'
                 }
             >
-                <div className='relative aspect-[3/1] w-full'>
+                <div className='relative aspect-3/1 w-full'>
                     <Logo className='object-contain px-6 sm:px-12' />
                 </div>
                 <h1 className='text-3xl font-extrabold text-login text-center tracking-tight'>
@@ -56,16 +58,17 @@ export default function LoginPage({title, description, redirectURL, version, btg
                         </button>
                     </form>
                 ) : (
-                    <Link href={redirectURL} className='w-full flex justify-center cursor-default'>
-                        <button
-                            className={
-                                'flex items-center justify-center gap-2 w-full max-w-xs py-3 px-6 rounded-xl bg-login font-bold text-lg ' +
-                                'hover:bg-login/80 transition-all duration-200 mb-2 mt-2 cursor-pointer'
-                            }
-                        >
-                            Login
-                            <LogIn className='w-6 h-6' />
-                        </button>
+                    <Link
+                        href={redirectURL}
+                        className={`
+                            flex items-center justify-center gap-2 w-full
+                            max-w-xs py-3 px-6 rounded-xl bg-login font-bold
+                            text-lg hover:bg-login/80 transition-all
+                            duration-200 mb-2 mt-2 cursor-pointer
+                        `}
+                    >
+                        Login
+                        <LogIn className='w-6 h-6' />
                     </Link>
                 )}
                 <span className='text-sm mt-2'>v{version}</span>
