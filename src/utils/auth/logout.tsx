2a15bd7e7aed9da2f2be3ace6e4a7d35ae65e592
eirikhanasand <eirik.hanasand@gmail.com>
2025-12-03 16:02:21 +0100

    Reverted handler

--- src/utils/auth/logout.tsx
+++ src/utils/auth/logout.tsx
@@ -1,17 +1,21 @@
-import { removeCookies } from '@utils/cookies/cookies'
 import { NextResponse } from 'next/server'
 import type { AuthLogoutProps } from 'uibee/utils'
 
 export default async function AuthLogout({ frontendURL, path }: AuthLogoutProps) {
-    removeCookies(
+    const response = NextResponse.redirect(new URL(path || '/', frontendURL))
+
+    const cookiesToRemove = [
         'access_token',
         'user_id',
         'user_name',
         'user_nickname',
         'user_email',
         'user_groups'
-    )
+    ]
 
-    const response = NextResponse.redirect(new URL(path || '/', frontendURL))
+    cookiesToRemove.forEach(cookieName => {
+        response.cookies.delete(cookieName)
+    })
+
     return response
 }
