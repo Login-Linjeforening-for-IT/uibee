96f1c487dda51f80a693f5010de2439a9bc3c037
0herm <0herm@protonmail.com>
2025-12-03 16:13:45 +0100

    fix

--- dist/src/utils/auth/logout.js
+++ dist/src/utils/auth/logout.js
@@ -1,7 +1,16 @@
-import { removeCookies } from '../cookies/cookies';
 import { NextResponse } from 'next/server';
 export default async function AuthLogout({ frontendURL, path }) {
-    removeCookies('access_token', 'user_id', 'user_name', 'user_nickname', 'user_email', 'user_groups');
     const response = NextResponse.redirect(new URL(path || '/', frontendURL));
+    const cookiesToRemove = [
+        'access_token',
+        'user_id',
+        'user_name',
+        'user_nickname',
+        'user_email',
+        'user_groups'
+    ];
+    cookiesToRemove.forEach(cookieName => {
+        response.cookies.delete(cookieName);
+    });
     return response;
 }
