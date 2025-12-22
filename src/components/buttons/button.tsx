554abafd5f92a25ea336d9d4289e1a49c9edb564
eirikhanasand <eirik.hanasand@gmail.com>
2025-12-22 20:41:37 +0100

    fixed styling

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -23,7 +23,7 @@ export default function Button({
     disabled
 }: ButtonProps) {
     const bg = color === 'secondary'
-        ? 'bg-login-600/70 outline-login-600 hover:bg-login-600/90'
+        ? 'bg-login-500/70 outline-login-500 hover:bg-login-500/90'
         : 'bg-login/70 outline-login hover:bg-login/90'
 
     if (!path) {
@@ -34,7 +34,7 @@ export default function Button({
                 onClick={onClick}
                 aria-label={text}
                 className={`
-                    ${bg} cursor-pointer px-4 rounded-md h-6 flex
+                    ${bg} cursor-pointer px-4 rounded-md min-h-8 h-8 flex
                     justify-evenly items-center gap-2 select-none
                     focus:outline-none border-0 outline w-fit ${className}
                 `}
@@ -49,7 +49,7 @@ export default function Button({
         return (
             <div
                 className={`
-                    ${bg} cursor-not-allowed px-4 rounded-md h-6 flex
+                    ${bg} cursor-not-allowed px-4 rounded-md h-8 flex
                     justify-evenly items-center gap-2 select-none
                 `}
             >
@@ -63,7 +63,7 @@ export default function Button({
         <Link
             href={path}
             className={`
-                ${bg} cursor-pointer px-4 rounded-md h-6 flex
+                ${bg} cursor-pointer px-4 rounded-md h-8 flex
                 justify-evenly items-center gap-2 select-none
             `}
         >
