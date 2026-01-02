885fc8400865c587f7b67018e89dee6ff9fbcd0f
0herm <0herm@protonmail.com>
2026-01-02 14:42:46 +0100

    fix prefix

--- dist/src/components/inputs/switch.js
+++ dist/src/components/inputs/switch.js
@@ -4,7 +4,7 @@ export default function Switch({ label, name, checked, onChange, className, disa
     return (_jsx(SelectionWrapper, { label: label, name: name, required: required, info: info, error: error, hideError: switchOnly, className: className, disabled: disabled, children: _jsxs("label", { className: `relative inline-flex items-center cursor-pointer ${switchOnly ? 'h-fit' : 'h-10.5'}`, children: [_jsx("input", { type: 'checkbox', id: name, name: name, checked: checked, onChange: onChange, disabled: disabled, required: required, className: 'sr-only peer' }), _jsx("div", { className: `
                     w-11 h-6 bg-login-500/50 rounded-full peer 
                     peer-checked:after:translate-x-full peer-checked:after:border-white 
-                    after:content-[''] after:absolute ${switchOnly ? 'top-0.5' : 'top-2.75'} after:left-0.5 
+                    after:content-[''] after:absolute ${switchOnly ? 'after:top-0.5' : 'after:top-2.75'} after:left-0.5 
                     after:bg-white after:border-gray-300 after:border after:rounded-full 
                     after:h-5 after:w-5 after:transition-all peer-checked:bg-login
                     ${disabled ? 'opacity-50 cursor-not-allowed' : ''}
--- src/components/inputs/switch.tsx
+++ src/components/inputs/switch.tsx
@@ -51,7 +51,7 @@ export default function Switch({
                 <div className={`
                     w-11 h-6 bg-login-500/50 rounded-full peer 
                     peer-checked:after:translate-x-full peer-checked:after:border-white 
-                    after:content-[''] after:absolute ${switchOnly ? 'top-0.5' : 'top-2.75'} after:left-0.5 
+                    after:content-[''] after:absolute ${switchOnly ? 'after:top-0.5' : 'after:top-2.75'} after:left-0.5 
                     after:bg-white after:border-gray-300 after:border after:rounded-full 
                     after:h-5 after:w-5 after:transition-all peer-checked:bg-login
                     ${disabled ? 'opacity-50 cursor-not-allowed' : ''}
