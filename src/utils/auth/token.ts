c327a0f6571dec4c7aebf3885f3cf7d3e0c263c7
0herm <0herm@protonmail.com>
2026-01-20 12:15:21 +0100

    add redirect after login logic

--- src/utils/auth/token.ts
+++ src/utils/auth/token.ts
@@ -5,12 +5,15 @@ export default async function AuthToken({ req, frontendURL, redirectPath }: Auth
     const url = new URL(req.url)
     const token = url.searchParams.get('access_token')
     const btg = url.searchParams.get('btg')
+    const redirectCookie = req.cookies.get('redirect_after_login')
+    const redirect = redirectCookie ? redirectCookie.value : redirectPath || '/'
+
     if (!token) {
         return NextResponse.json({ error: 'No access token provided' }, { status: 400 })
     }
 
     if (btg) {
-        return NextResponse.redirect(new URL(redirectPath || '/', frontendURL))
+        return NextResponse.redirect(new URL(redirect, frontendURL))
     }
 
     const accessToken = url.searchParams.get('access_token')!
@@ -20,13 +23,17 @@ export default async function AuthToken({ req, frontendURL, redirectPath }: Auth
     const userEmail = url.searchParams.get('email')!
     const userGroups = url.searchParams.get('groups')!
 
-    const response = NextResponse.redirect(new URL(redirectPath || '/', frontendURL))
+    const response = NextResponse.redirect(new URL(redirect, frontendURL))
     response.cookies.set('access_token', accessToken)
     response.cookies.set('user_id', userID)
     response.cookies.set('user_name', username)
     response.cookies.set('user_nickname', userNickname)
     response.cookies.set('user_email', userEmail)
     response.cookies.set('user_groups', userGroups)
+
+    if (redirectCookie) {
+        response.cookies.delete('redirect_after_login')
+    }
 
     return response
 }
