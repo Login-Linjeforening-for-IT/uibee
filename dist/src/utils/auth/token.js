b715bdca1dae608a07e74ad6c438b8b3acecc822
0herm <0herm@protonmail.com>
2026-01-20 12:15:49 +0100

    v2.8.2

--- dist/src/utils/auth/token.js
+++ dist/src/utils/auth/token.js
@@ -3,11 +3,13 @@ export default async function AuthToken({ req, frontendURL, redirectPath }) {
     const url = new URL(req.url);
     const token = url.searchParams.get('access_token');
     const btg = url.searchParams.get('btg');
+    const redirectCookie = req.cookies.get('redirect_after_login');
+    const redirect = redirectCookie ? redirectCookie.value : redirectPath || '/';
     if (!token) {
         return NextResponse.json({ error: 'No access token provided' }, { status: 400 });
     }
     if (btg) {
-        return NextResponse.redirect(new URL(redirectPath || '/', frontendURL));
+        return NextResponse.redirect(new URL(redirect, frontendURL));
     }
     const accessToken = url.searchParams.get('access_token');
     const userID = url.searchParams.get('id');
@@ -15,12 +17,15 @@ export default async function AuthToken({ req, frontendURL, redirectPath }) {
     const userNickname = url.searchParams.get('username');
     const userEmail = url.searchParams.get('email');
     const userGroups = url.searchParams.get('groups');
-    const response = NextResponse.redirect(new URL(redirectPath || '/', frontendURL));
+    const response = NextResponse.redirect(new URL(redirect, frontendURL));
     response.cookies.set('access_token', accessToken);
     response.cookies.set('user_id', userID);
     response.cookies.set('user_name', username);
     response.cookies.set('user_nickname', userNickname);
     response.cookies.set('user_email', userEmail);
     response.cookies.set('user_groups', userGroups);
+    if (redirectCookie) {
+        response.cookies.delete('redirect_after_login');
+    }
     return response;
 }
